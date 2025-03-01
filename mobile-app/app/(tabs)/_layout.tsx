8c1ba2e9c27ab4b05bf7c4e7a92782d3cce4ee4a
harrys522 <hsackman@protonmail.com>
2025-02-28 22:18:49 -0700

    Fixed some styling and tried to fix tabs

--- mobile-app/app/(tabs)/_layout.tsx
+++ mobile-app/app/(tabs)/_layout.tsx
@@ -30,14 +30,14 @@ export default function TabLayout() {
           }),
         }}>
         <Tabs.Screen
-          name="profiles/index"
+          name="profiles"
           options={{
             title: 'Profiles',
             tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
           }}
         />
         <Tabs.Screen
-          name="matches/index"
+          name="matches"
           options={{
             title: 'Matches',
             tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
